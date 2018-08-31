dc8c6f02c75eb5e42a0976d17072440d0c0ebf7a
hrtk91 <ht91218@gmail.com>
2018-08-31 23:21:53 +0900

    ルーティング前のバリデートCbk追加とデフォルトルーティングの追加

--- Rooter.js
+++ Rooter.js
@@ -1,38 +1,72 @@
 'use strict';
 
 function Rooter() {
-    this._listener = [];
+    this.listener = [];
+    this.validations = [];
 }
 
-Rooter.prototype.on = function (type, callback) {
-    let listener = this._listener;
-    if (listener[type] === undefined) {
-        if (callback !== undefined) {
-            listener[type] = callback;
-        } else {
-            return new Promise(resolve => listener[type] = resolve);
-        }
+Rooter.prototype.on = function (type) {
+    var validator;
+    var callback;
+    if (typeof(arguments[1]) === 'function' && typeof(arguments[2]) === 'function') {
+        validator = arguments[1];
+        callback = arguments[2];
+    } else if (typeof(arguments[2]) !== 'function' && typeof(arguments[1]) === 'function') {
+        callback = arguments[1];
     } else {
-        throw 'already pushed';
+        throw new Error('Argument exception');
     }
-    
+
+    const validations = this.validations;
+    const listener = this.listener;
+
+    if (typeof(validator) === 'function')
+        validations[type] = validator;
+    else
+        validations[type] = undefined;
+
+    listener[type] = callback;
+
     return this;
 }
 Rooter.prototype.off = function (type) {
-    let listener = this._listener;
-    if (listener[type] !== undefined) {
+    const validations = this.validations;
+    const listener = this.listener;
+
+    if (validations[type] !== undefined)
+        validations[type] = undefined;
+    if (listener[type] !== undefined)
         lsitener[type] = undefined;
-    }
 
     return this;
 }
 Rooter.prototype.fire = function (type, option) {
-    let listener = this._listener;
+    const validator = this.validations[type];
+    const listener = this.listener[type];
     option = option || {};
-    if (listener[type] !== undefined) {
-        listener[type](option);
+
+    if (typeof(listener) !== 'function') {
+        if (this.defaultfunc)
+            this.defaultfunc(option);
+        return this;
+    }
+    
+    if (typeof(validator) !== 'function') {
+        listener(option);
+        return this;
     }
 
+    const canExecuteCbk = validator(option);
+    if (canExecuteCbk)
+        listener(option);
+
+    return this;
+}
+
+Rooter.prototype.defaultRooting = function (defaultfunc) {
+    if (typeof(defaultfunc) === 'function')
+        this.defaultfunc = defaultfunc;
+    
     return this;
 }
 
