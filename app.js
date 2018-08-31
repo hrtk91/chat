5031190b043409ebfaa8a8e3f04fb39e95b8b78f
hrtk91 <ht91218@gmail.com>
2018-08-31 23:23:18 +0900

    サーバ処理整理

--- app.js
+++ app.js
@@ -8,29 +8,15 @@ const http = require('http');
 const cookie = require('cookie');
 const Rooter = require('./Rooter.js');
 const ChatDB = require('./ChatDB.js');
-
-const db = new ChatDB({
-    host: 'localhost',
-    port: 3306,
-    user: 'node',
-    password: 'node',
-    database: 'chat',
-});
-db.connect()
-.then(err => {
-    console.info('ChatDB connect success');
-})
-.catch(err => {
-    console.error(err);
-});
+const chatController = require('./ChatController.js');
 
 http.createServer(main).listen(8080);
 
 function main(req, res) {
-    let query = url.parse(req.url, true);
-    let pathname = query.pathname;
-    let filepath = path.join(__dirname, pathname);
-    let ext = path.extname(filepath);
+    const query = url.parse(req.url, true);
+    const pathname = query.pathname;
+    const filepath = path.join(__dirname, pathname);
+    const ext = path.extname(filepath);
     
     console.log('requested:' + req.connection.remoteAddress + '@' + filepath);
 
@@ -40,177 +26,11 @@ function main(req, res) {
         return;
     }
 
-    // apiへのルーティングコード書くこと
-    const rooter = new Rooter();
-
-    rooter.on('/')
-    .then(option => {
-        let filepath = path.join(__dirname, 'index.html');
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        writeResponse(filepath, res);
-    });
-
-    rooter.on('/user/create')
-    .then(option => {
-        if (req.method !== 'POST') {
-            res.writeHead(400, {'Content-Type': 'text/plain'});
-            res.end('400 Bad Request. Please use POST method.');
-        }
-
-        let session = cookie.parse(req.headers.cookie);
-        const username = session.username;
-        const password = session.password;
-
-        return db.createUser(username, password);
-    })
-    .then(() => {
-        res.writeHead(201, {'Content-Type': 'application/json'});
-        res.end();
-    })
-    .catch(err => {
-        res.writeHead(500, {'Content-Type': 'text/plain'});
-        res.end('500 Server Internal Error.');
-        console.error(err);
-    });
-
-    rooter.on('/user/login')
-    .then(option => {
-        if (req.method !== 'GET') {
-            res.writeHead(400, {'Content-Type': 'text/plain'});
-            res.end('400 Bad Request. Please use GET method.');
-            throw new Error('bad request');
-        }
-
-        let session = cookie.parse(req.headers.cookie);
-        const username = session.username;
-        const password = session.password;
-
-        return db.login(username, password);
-    })
-    .then(() => {
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        res.end();
-    })
-    .catch(err => {
-        res.writeHead(500, {'Content-Type': 'text/plain'});
-        res.end('500 Server Internal Error.');
-        console.error(err);
-    });
-
-    rooter.on('/article')
-    .then(option => {
-        if (req.method === 'POST' || req.method === 'PUT') {
-            let body = '';
-            req.on('data', (chunk) => {
-                body += chunk;
-                // 1e6 = 10^6 = 1MB
-                if (body >= 1e6) req.connection.destroy();
-            });
-            req.on('error', (err) => {
-                res.writeHead(500, {'Content-Type': 'text/plain'});
-                res.end('500 Server Internal Error.');
-                console.error(err.message);
-            });
-            req.on('end', () => {
-                const post = JSON.parse(body);
-
-                let session = cookie.parse(req.headers.cookie);
-                const username = session.username;
-                const password = session.password;
-        
-                db.login(username, password)
-                .then(results => {
-                    return db.post({
-                        sender: post.sender,
-                        message: post.message
-                    });    
-                })
-                .then(id => {
-                    res.writeHead(201, {'Content-Type': 'application/json'});
-                    res.end();
-                })
-                .catch(err => {
-                    res.writeHead(500, {'Content-Type': 'text/plain'});
-                    res.end('500 Server Internal Error.');
-                    console.error(err);
-                });
-            });
-        } else {
-            res.writeHead(400, {'Content-Type': 'text/plain'});
-            res.end('400 Bad Request. Please use POST or PUT method.');
-        }
-    });
-
-    rooter.on('/articles')
-    .then(option => {
-        if (req.method !== 'GET') {
-            res.writeHead(400, {'Content-Type': 'text/plain'});
-            res.end('400 Bad Request. Please use GET method.');
-            return;
-        }
-        const parsedurl = url.parse(req.url, true);
-        return db.getArticles(parsedurl.query);
-    })
-    .then((results, fields) => {
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        res.end(JSON.stringify(results));
-    })
-    .catch(err => {
-        res.writeHead(500, {'Content-Type': 'text/plain'});
-        res.end('500 Server Internal Error.');
-        console.error(err);
-    });
-
-    rooter.on('/image')
-    .then(option => {
-        const req = option.request || {};
-        const res = option.response || {};
-
-        if (req.method !== 'POST') {
-            res.writeHead(400, { 'Content-Type:': 'text/plain' });
-            res.end('400 Bad Request. Please use POST method.');
-            return;
-        }
-
-        let body = '';
-        req.on('data', (chunk) => {
-            body += chunk;
-            if (body.length >= 10e6) req.connection.destroy();
-        });
-        req.on('error', (err) => {
-            res.writeHead(500, {'Content-Type': 'text/plain'});
-            res.end('500 Server Internal Error.');
-            console.error(err);
-        });
-        req.on('end', () => {
-            const post = JSON.parse(body);
-            const user = post.sender;
-            const message = post.message;
-            const imageData = post.imageData;
-
-            db.postImage({
-                sender: user,
-                message: message,
-                data: imageData
-            })
-            .then(id=> {
-                res.writeHead(201, {'Content-Type': 'application/json'});
-                res.end();
-            })
-            .catch(err => {
-                res.writeHead(500, {'Content-Type': 'text/plain'});
-                res.end('500 Server Internal Error.');
-                console.error(err);
-            });
-        });
-    });
-
     const apicode = pathname;
-    rooter.fire(apicode, {
+    chatController.fire(apicode, {
         request: req,
         response: res
     });
-    // ここまで
 }
 
 function setHeaderFromExtension(ext, res) {
