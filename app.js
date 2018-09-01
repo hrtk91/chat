cf041b45062ce01b3c8b9d9adf57cd560ffd72ed
hrtk91 <ht91218@gmail.com>
2018-09-01 20:53:53 +0900

    modelフォルダ作成にともない修正

--- app.js
+++ app.js
@@ -6,14 +6,16 @@ const url = require('url');
 const path = require('path');
 const http = require('http');
 const cookie = require('cookie');
-const Rooter = require('./Rooter.js');
-const ChatDB = require('./ChatDB.js');
-const chatController = require('./ChatController.js');
+const Rooter = require('./Models/Rooter.js');
+const ChatDB = require('./Models/ChatDB.js');
+const chatController = require('./Controllers/ChatController.js');
 
 http.createServer(main).listen(8080);
 
 function main(req, res) {
     const query = url.parse(req.url, true);
+    
+    console.info((new Date()) + ' requested from "' + req.socket.address().address + '" : ' + query.pathname);
 
     const apicode = query.pathname;
     chatController.fire(apicode, {
@@ -21,28 +23,3 @@ function main(req, res) {
         response: res
     });
 }
-
-function setHeaderFromExtension(ext, res) {
-    switch (ext) {
-        case '.html':
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            break;
-        case '.js':
-            res.writeHead(200, {'Content-Type': 'application/json'});
-            break;
-        default:
-            res.writeHead(404, {'Content-Type': 'text/plain'});
-            break;
-    }
-}
-
-function writeResponse(filepath, res) {
-    fs.readFile(filepath, (err, data) => {
-        if (!err) {
-            res.end(data);
-        } else {
-            res.end('404 NotFound');
-        }
-    });
-}
-
