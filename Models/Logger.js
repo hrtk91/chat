4ccf3b1843f491defac47da8b0cd190b9424a756
hrtk91 <ht91218@gmail.com>
2018-09-30 19:54:15 +0900

    Logger仮作成

--- Models/Logger.js
+++ Models/Logger.js
@@ -1,33 +1,54 @@
 const fs = require('fs');
 const path = require('path');
 
-function Logger(filename) {
+function Logger(filename, interval) {
+    filename = filename || '';
+    interval = interval || 200;
     this.filepath = path.resolve(filename);
-    this.interval = 200;
-    this.timers = [];
+    this.interval = interval;
+    this.tasks = [];
 }
-
+Logger.prototype = Object.create(require('events').prototype);
 Logger.prototype.write = function write(body) {
-    return new Promise(resolve => {
-        fs.appendFile(this.filepath, body, (err) => {
+    return new Promise((resolve, reject) => {
+        fs.appendFile(this.filepath, body, err => {
             if (err) {
-                const id = setTimeout(() => {
-                    this.write(body);
-                    const idx = this.timers.indexOf(id);
-                    if (idx !== -1) {
-                        this.timers.slice(idx, 1);
+                console.error('Logger.write: start retry. body = "' + body + '", Error = "' + err.message + '"');
+                new Promise(fullfilled => {
+                    const id = setTimeout(() => {
+                        fullfilled(id);
+                    }, this.interval);
+                    const task = {
+                        timeoutId: id,
+                        body: body,
+                        retry: 0,
+                        limit: 5,
+                    };
+                    this.tasks.push(task);
+                })
+                .then(timeoutId => {
+                    const idx = this.tasks.map(t => t.timeoutId).indexOf(timeoutId);
+                    if (idx === -1) {
+                        reject(new Error('Logger.write: exception in retry.'));
                     }
-                }, this.interval);
-                this.timers.push(id);
-                return;
+                    const task = this.tasks[idx];
+                    this.tasks.slice(idx, 1);
+                    if (task.retry++ < task.limit) {
+                        this.emit('retryed', task);
+                        this.write(body).then(resolve);
+                    } else {
+                        reject(new Error('Logger.write: exceeded retry limit. body = "' + task.body + '"'));
+                    }
+                });
+            } else {
+                resolve();
             }
-            resolve();
         });
     });
 }
 
 Logger.prototype.close = function close() {
-    this.timers.forEach(timerId => {
+    this.tasks.forEach(timerId => {
         clearTimeout(timerId);
     });
 }
--- spec/Logger.spec.js
+++ spec/Logger.spec.js
@@ -3,16 +3,15 @@ const path = require('path');
 const Logger = require(path.join(process.cwd(), '/Models/Logger.js'));
 
 describe('Logger.jsテスト', function () {
-    describe('write', function () {
+    describe('Logger.write', function () {
         const testFile = 'test.txt';
         
-        beforeAll(done => {
-            fs.unlink(testFile, function (err) {
-                if (err) {
-                    console.log('beforeAll error.');
-                } else {
+        beforeEach(done => {
+            fs.exists(testFile, isExists => {
+                if (!isExists) return done();
+                fs.unlink(testFile, function (err) {
                     done();
-                }
+                });
             });
         });
 
@@ -26,6 +25,51 @@ describe('Logger.jsテスト', function () {
                     }
                     expect(str).toBe(data);
                     done();
+                });
+            });
+        });
+
+        it('書き込み権限制限ファイルに書き込み', function (done) {
+            const str = 'test word';
+            new Promise((resolve, reject) => {
+                fs.writeFile(testFile, '', err => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve();
+                    }
+                });
+            })
+            .then(_ => {
+                return new Promise((resolve, reject) => {
+                    fs.chmod(testFile, 0o444, err => {
+                        if (err) {
+                            reject(err);
+                        } else {
+                            resolve();
+                        }
+                    });
+                });
+            })
+            .then(_ => {
+                const logger = new Logger(testFile);
+                let retry = 0;
+                logger.on('retryed', function retryed() {
+                    if (retry < 1) {
+                        retry++;
+                        return;
+                    }
+                    fs.chmod(testFile, 0o666, err => {
+                        if (err) return console.log(err);
+                        logger.removeListener('retryed', retryed);
+                    });
+                });
+                logger.write(str).then(_ => {
+                    fs.readFile(testFile, 'utf8', (err, data) => {
+                        expect(1).toBe(retry);
+                        expect(str).toBe(data);
+                        done();
+                    });
                 });
             });
         });
